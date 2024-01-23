66d4f2d33d0828804dbf1acce096d77b8d2555c2
John Pieri <jvpieri@gmail.com>
2024-01-22 23:07:04 -0400

    Initial Commit

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCeJy4Od6TQTOFViE-H8AEAv5yLsXrAxm4",
+  authDomain: "react-notes-11c5b.firebaseapp.com",
+  projectId: "react-notes-11c5b",
+  storageBucket: "react-notes-11c5b.appspot.com",
+  messagingSenderId: "550150403896",
+  appId: "1:550150403896:web:30cb0f057afcbb678ef3cd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
