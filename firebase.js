675dde88ba077160641896309b458b53b87547b1
John Pieri <jvpieri@gmail.com>
2024-01-23 22:50:56 -0400

    Initilized App and set up database.

--- firebase.js
+++ firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore, collection } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCeJy4Od6TQTOFViE-H8AEAv5yLsXrAxm4",
@@ -6,8 +7,11 @@ const firebaseConfig = {
   projectId: "react-notes-11c5b",
   storageBucket: "react-notes-11c5b.appspot.com",
   messagingSenderId: "550150403896",
-  appId: "1:550150403896:web:30cb0f057afcbb678ef3cd"
+  appId: "1:550150403896:web:30cb0f057afcbb678ef3cd",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const database = getFirestore(app);
+const notesCollection = collection(database, "Notes")
+console.log(notesCollection)
